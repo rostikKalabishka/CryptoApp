c327286abc266f35f8f5d83188dec10b28d7a9a7
rostikKalabishka <rostik31052002@gmail.com>
2023-11-22 15:30:08 +0200

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:crypto_app/crypto_app.dart';
+import 'package:crypto_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const CryptoApp());
 }
