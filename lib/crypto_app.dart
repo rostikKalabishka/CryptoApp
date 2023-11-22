65decd46507eb44fe593b0b8c9b93777606e3373
rostikKalabishka <rostik31052002@gmail.com>
2023-11-22 14:57:05 +0200

    first commit

---
+++ lib/crypto_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
