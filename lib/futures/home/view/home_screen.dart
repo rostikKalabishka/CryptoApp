98142abf3f8bd2123fe3ad0a59debb6715347505
rostikKalabishka <rostik31052002@gmail.com>
2023-12-13 18:33:18 +0200

    change

--- lib/futures/home/view/home_screen.dart
+++ lib/futures/home/view/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:crypto_app/ui/theme/const.dart';
+
 import 'package:flutter/material.dart';
 
 import '../../crypto_coin_list/view/view.dart';
