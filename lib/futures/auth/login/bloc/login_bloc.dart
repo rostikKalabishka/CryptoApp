fd352a14bfbcc4fc46a98fd32d12a391195bb8dd
rostikKalabishka <rostik31052002@gmail.com>
2023-12-13 18:35:37 +0200

    test

--- lib/futures/auth/login/bloc/login_bloc.dart
+++ lib/futures/auth/login/bloc/login_bloc.dart
@@ -7,7 +7,7 @@ part 'login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginInitial()) {
     on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+      // test
     });
   }
 }
